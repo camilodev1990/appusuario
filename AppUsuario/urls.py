1699f720c361e8cbcfd6edd148a7a4c048287a39
camilo pereira <camilodev1990@gmail.com>
2025-05-10 10:11:58 -0500

    modifico views y url con logout

--- AppUsuario/urls.py
+++ AppUsuario/urls.py
@@ -6,4 +6,5 @@ from apps_importadas.appusuario.AppUsuario import views
 urlpatterns = [
     path('registro/', views.RegistroTemplateView.as_view(), name='registro'),
     path('login/', views.LoginTemplateView.as_view(), name=('login')),
+    path('logout/', views.LogoutTemplateView.as_view(), name='logout'),
 ]
